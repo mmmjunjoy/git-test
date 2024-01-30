2af1d590083f49c282bf08d2c30ff6dcbfe7dcb8
mmmjunjoy <sjbssk0809@gmail.com>
2024-01-30 20:39:07 +0900

    형상관리 - cherry-pick  & rebase

--- gitstash/git1.py
+++ gitstash/git1.py
@@ -1,3 +1,11 @@
+# 최종적으로,
+# main을 깔끔하게 pull받은후, 
+# REAL branch에 temp에서작업한 내용을 옮긴후 push를 해줘었다.
+
+# 사용한 내역은 , 1. git rebase -i HEAD~ 
+                # 2. git cherry-pick [code번호] -n
+  
+
 #연습파일
 
 def math():
